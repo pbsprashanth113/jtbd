078e1044597be19dedce2da874462fe00a7bb19d
pbs <pbs@113industries.com>
2024-03-19 19:14:27 +0530

    initial setup

--- how-to-bind-data-to-the-vue-heatmap-chart-component/src/App.vue
+++ how-to-bind-data-to-the-vue-heatmap-chart-component/src/App.vue
@@ -1,19 +1,24 @@
 <script setup>
 import { provide } from 'vue';
 import { HeatMapComponent as EjsHeatmap, Adaptor,  Tooltip, Legend } from "@syncfusion/ej2-vue-heatmap";
-import situation_needs_vs_posts  from './assets/situation_needs_vs_posts.json' 
-import situations_vs_posts  from './assets/situation_vs_posts_heatmap.json' 
+import situation_need_vs_post  from './assets/situation_needs_vs_posts.json' 
+import situation_vs_post  from './assets/situation_vs_posts_heatmap.json' 
 import situation_need_vs_situation_need  from './assets/cosine_sim_matrix_situation_need_vs_situation_need.json' 
+import situation_vs_situation from './assets/cosine_sim_matrix_situation_vs_situation.json' 
 
-const posts = situation_needs_vs_posts["posts"]
-const situation_needs = situation_needs_vs_posts["situation_needs"]
-const situations = situations_vs_posts["situation"]
-const _situation_need = situation_need_vs_situation_need["situation_needs"]
+const situation_need_vs_posts__posts = situation_need_vs_post["posts"]
+const situation_need_vs_posts__situation_needs = situation_need_vs_post["situation_needs"]
+const situation_vs_posts_posts = situation_vs_post["posts"]
+const situation_vs_posts_situation = situation_vs_post["situation"]
+const _situation_need_vs_situation_need = situation_need_vs_situation_need["situation_needs"]
+const _situation_vs_situation = situation_vs_situation["situation"]
 
-const xAxis = { labels : posts, visible: true}
-const yAxis_situations = { labels : situations, visible: true}
-const yAxis = { labels : situation_needs, visible: true}
-const situation_need_vs_situation_need_xAxis_yAxis= { labels : _situation_need, visible: true}
+// const situation_need_vs_posts_xAxis = { labels : posts, visible: true}
+// const situation_need_vs_posts_yAxis = { labels : posts, visible: true}
+
+// const situation_yAxis = { labels : situations, visible: true}
+// const situation_need_yAxis = { labels : situation_needs, visible: true}
+// const situation_need_vs_situation_need_yAxis= { labels : situation_need, visible: true}
 
 const paletteSettings = {
             palette: [
@@ -60,18 +65,23 @@ const showTooltip = true
 </script>
 <template>
         <div style="padding-top: 10pt;">
-                  <h1>Situation and Needs Vs Posts Chart</h1>
-                  <ejs-heatmap id="situation_needs_vs_posts" :dataSource="situation_needs_vs_posts['cosine_sim_matrix']" :xAxis="xAxis" :yAxis="yAxis" 
+                  <h1>Situation and Needs Vs Posts </h1>
+                  <ejs-heatmap id="situation_needs_vs_posts" :dataSource="situation_need_vs_post['cosine_sim_matrix']" :xAxis="{ labels : situation_need_vs_posts__posts, visible: true}" :yAxis="{ labels : situation_need_vs_posts__situation_needs, visible: true}" 
                   height="4000px" width="4000px"   :paletteSettings="paletteSettings"  :tooltipSettings="tooltipSettings"></ejs-heatmap>
         </div>
         <div style="padding-top: 10pt;">
-                  <h1>Situation vs Posts Chart</h1>
-                  <ejs-heatmap   id="situations_vs_posts" :dataSource="situations_vs_posts['cosine_sim_matrix']" :xAxis="xAxis" :yAxis="yAxis_situations" 
+                  <h1>Situation vs Posts </h1>
+                  <ejs-heatmap   id="situations_vs_posts" :dataSource="situation_vs_post['cosine_sim_matrix']" :xAxis="{ labels : situation_vs_posts_posts, visible: true}" :yAxis="{ labels :  situation_vs_posts_situation, visible: true}" 
                   height="4000px" width="4000px"   :paletteSettings="paletteSettings"  :tooltipSettings="tooltipSettings"></ejs-heatmap>
         </div>
         <div style="padding-top: 10pt;">
-                  <h1>Situation Needs vs Situation Needs</h1>
-                  <ejs-heatmap   id="situation_need_vs_situation_need" :dataSource="situation_need_vs_situation_need['cosine_sim_matrix']" :xAxis="xAxis" :yAxis="situation_need_vs_situation_need_xAxis_yAxis" 
+                  <h1>Situation Need vs Situation Need</h1>
+                  <ejs-heatmap   id="situation_need_vs_situation_need" :dataSource="situation_need_vs_situation_need['cosine_sim_matrix']" :xAxis="{ labels : _situation_need_vs_situation_need, visible: true}" :yAxis="{ labels : _situation_need_vs_situation_need, visible: true}" 
+                  height="4000px" width="4000px"   :paletteSettings="paletteSettings"  :tooltipSettings="tooltipSettings"></ejs-heatmap>
+        </div>
+        <div style="padding-top: 10pt;">
+                  <h1>Situation  vs Situation </h1>
+                  <ejs-heatmap   id="situation_vs_situation" :dataSource="situation_vs_situation['cosine_sim_matrix']" :xAxis=" { labels : _situation_vs_situation , visible: true}" :yAxis="{ labels : _situation_vs_situation , visible: true}" 
                   height="4000px" width="4000px"   :paletteSettings="paletteSettings"  :tooltipSettings="tooltipSettings"></ejs-heatmap>
         </div>
 </template>
